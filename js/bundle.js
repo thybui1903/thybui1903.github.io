65f4fb620bd3bff8b7f153948890020c7b1e1b5a
thybui1903 <thybui1903qn@gmail.com>
2026-05-27 10:19:14 +0700

    modified:   js/bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -479,12 +479,20 @@ class SectionManager {
                 <summary class="experience-header">
                     <div class="experience-header-content">
                         <h3>${company}</h3>
-                        <span>${role}</span>
+                        <p class="experience-role">${role}</p>
                         ${date}
                     </div>
                     ${logoHtml}
                     <div class="accordion-toggle" aria-hidden="true">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round">
                             <polyline points="6,9 12,15 18,9"></polyline>
                         </svg>
                     </div>
